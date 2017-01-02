d6179f580787ff41a0b0f4a625b2817bc8a9cabd
Jennifer Christie Christie <jennifer.christie1@gmail.com>
2017-01-02 15:10:57 -0500

    video22 animating the order in and out and animating the amount of seafood

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,9 +1,7 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-    apiKey: "AIzaSyCSflICDC_Gdf-UVXihUbc9ul6IVrhhVqU",
-    authDomain: "catch-of-the-day-j-chris.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-j-chris.firebaseio.com",
+
 });
 
 export default base;
