048764dea193a410d13c98cd09d13572a825b681
Jennifer Christie Christie <jennifer.christie1@gmail.com>
2017-01-02 11:42:04 -0500

    video 20 be able to edit from the website and firebase and it all syncs

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,10 +2,49 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+  constructor() {
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, key) {
+    const fish =this.props.fishes[key];
+    // console.log(fish);
+    // take a copy of that fish and update it with the new data
+    // const updatedFish = Object.assign({}, fish)
+    // ⬆ or ⬇ works too
+    const updatedFish = {
+      ...fish,
+      [e.target.name]: e.target.value
+    }
+    this.props.updateFish(key, updatedFish);
+    console.log(e.target);
+  }
+
+  renderInventory(key) {
+    const fish = this.props.fishes[key];
+    return (
+      <div className="fish-edit" key={key}>
+        <input type="text" name="name" value={fish.name} placeholder="Fish Name" onChange={(e) => this.handleChange(e, key)} />
+        <input type="text" name="price" value={fish.price} placeholder="Fish Price" onChange={(e) => this.handleChange(e, key)} />
+
+        <select type="text" name="status" value={fish.status} placeholder="Fish Status" onChange={(e) => this.handleChange(e, key)}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+
+        <textarea type="text" name="desc" value={fish.desc} placeholder="Fish Desc" onChange={(e) => this.handleChange(e, key)}></textarea>
+        <input type="text" name="image" value={fish.image} placeholder="Fish Image" onChange={(e) => this.handleChange(e, key)} />
+      </div>
+    )
+  }
+
   render(){
     return (
       <div>
       <h2>Inventory</h2>
+      {Object.keys(this.props.fishes).map(this.renderInventory)}
       <AddFishForm addFish={this.props.addFish} />
     {/*This is how to get the sample fish from the sample-fishes.js to load in the state in the props(console also line Inventoty props line 48(loadSample))*/}
       <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
