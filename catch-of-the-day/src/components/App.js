4412dc976f1e177dae697dcab5a8a153e9774875
Jennifer Christie Christie <jennifer.christie1@gmail.com>
2017-01-02 12:21:35 -0500

    video22 Deleting from order and inventory

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,9 +12,11 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
     // initial state (lines 8-31, 40 like partials where we don't have to edit text the has the same info everytime we want to change something?) also in Inventory.js line 9
     this.state = {
       fishes: {},
@@ -70,6 +72,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -82,6 +90,12 @@ class App extends React.Component {
     //update or add the new number of fish ordered
     order[key] = order[key] + 1 || 1;
     // update our state
+    this.setState({ order });
+  }
+
+  removeFromOrder(key) {
+    const order = {...this.state.order};
+    delete order[key];
     this.setState({ order });
   }
 
@@ -103,9 +117,11 @@ class App extends React.Component {
         fishes={this.state.fishes}
         order={this.state.order}
         params={this.props.params}
+        removeFromOrder={this.removeFromOrder}
         />
         <Inventory
         addFish={this.addFish}
+        removeFish={this.removeFish}
         loadSamples={this.loadSamples}
         fishes={this.state.fishes}
         updateFish={this.updateFish}
